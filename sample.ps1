2c25806d3c007b722ddbc851ace2f9087c10976b
saurabhdhiraj <saurabhdhiraj@gmail.com>
2019-09-18 13:20:59 +0530

    Update sample.ps1

--- sample.ps1
+++ sample.ps1
@@ -1,2 +1,4 @@
-mkdir C:\temp
-echo "Hello World" > C:\temp\file.txt
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\services\SharedAccess\Parameters\FirewallPolicy\DomainProfile' -name "EnableFirewall" -Value 0
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\services\SharedAccess\Parameters\FirewallPolicy\PublicProfile' -name "EnableFirewall" -Value 0
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\services\SharedAccess\Parameters\FirewallPolicy\Standardprofile' -name "EnableFirewall" -Value 0 
+Restart-Service -Name mpssvc
